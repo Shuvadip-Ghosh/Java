378d87e831750fc76d3928517cc4872ff7a5913e
Shuvadip Ghosh <shuvadipdeveloper@gmail.com>
2022-09-02 15:22:49 +0530

    new program

---
+++ patterns/Pattern1.java
@@ -0,0 +1,37 @@
+// Wap a program to print this pattern
+// 70 71 72 73 74
+//    78 77 76 75
+//       79 80 81
+//          82 83
+//             84
+
+public class Pattern1 {
+
+    public static void main(String[] args) {
+        int n = 70,s=3,l=5,num=0;
+        for (int i = 1; i <=5; i++) {
+            for (int j = 1; j <i; j++) {
+                System.out.print("   ");
+            }
+            for (int j = l; j >=1; j--) {
+                if (i % 2 != 0) {
+                    System.out.print(n+" ");
+                    n++;
+                }
+                else{
+                    System.out.print(n+" ");
+                    n--;
+                }
+            }
+            if (i%2 !=0){
+                n = n+s;
+                num = n;
+                s-=2;
+            } 
+            else
+                n = num +1;
+            l--;
+            System.out.println();
+        }
+    }
+}
